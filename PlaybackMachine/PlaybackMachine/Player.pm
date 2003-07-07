247b3920af1c9a737349da7ba48217506abc95bd
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2003-07-07 23:29:22 +0000

    Initial revision

---
+++ PlaybackMachine/PlaybackMachine/Player.pm
@@ -0,0 +1,134 @@
+package Video::PlaybackMachine::Player;
+
+####
+#### Video::PlaybackMachine::Player
+####
+#### A POE::Session which displays movies and still frames onscreen
+#### based on events.
+####
+
+use strict;
+use base 'Exporter';
+our @EXPORT_OK = qw(PLAYER_STATUS_STOP PLAYER_STATUS_PLAY PLAYER_STATUS_STILL);
+
+use POE;
+use Xine_simple qw(:all);
+
+############################# Class Constants ################################
+
+## How often to check to see if Xine has stopped, in seconds
+use constant XINE_CHECK_INTERVAL_SECS => 2;
+
+## Status codes Xine will report
+use constant PLAYER_STATUS_STOP => 0;
+use constant PLAYER_STATUS_PLAY => 1;
+use constant PLAYER_STATUS_STILL => 2;
+
+############################## Class Methods #################################
+
+##
+## new()
+##
+## Returns a new instance of Player. Note that the session is not created
+## until you call spawn().
+##
+sub new {
+  my $type = shift;
+
+  bless {}, $type;
+}
+
+############################## Session Methods ###############################
+
+##
+## On session start, initializes Xine and prepares it to start playing.
+## The Xine screen will not appear until the first 'play' request.
+##
+sub _start {
+  my $kernel = $_[KERNEL];
+
+  $kernel->alias_set('Player');
+  xine_simple_init();
+
+}
+
+##
+## Clean up after Xine.
+##
+sub _stop {
+  xine_simple_cleanup();
+}
+
+##
+## Responds to a 'play' request by playing a movie on Xine.
+## Arguments:
+##   ARG0: $offset -- number of seconds after the movie's start to begin
+##   ARG1: @filenames -- ARG1 onward contains the files to play, in order.
+##
+## Currently, Xine_simple doesn't support playing multiple files, so
+## we'll just play the first one.
+##
+## After Xine is started, we'll check on it every $XINE_CHECK_INTERVAL
+## seconds to see if it has stopped.
+##
+## Currently Xine_simple doesn't report errors, but if it did we'd send
+## an 'Error' event to Logger with what had happened.
+##
+## TODO: We should add a check to see if Xine is already running and report
+##       an error if so.
+##
+sub play {
+  my ($kernel, $offset, $file) = @_[KERNEL, ARG0, ARG1 ];
+
+  defined $file or die "No files specified! stopped";
+
+  xine_simple_play($file, $offset);
+
+  $kernel->delay( 'check_finished', XINE_CHECK_INTERVAL_SECS );
+
+}
+
+##
+## Responds to a 'check_finished' request by checking to see if
+## Xine is playing a movie. If not, fires a 'finished' event at the scheduler.
+## If so, tells itself to check again in XINE_CHECK_INTERVAL_SECS seconds.
+##
+sub check_finished {
+  my ($self, $kernel) = @_[OBJECT, KERNEL];
+
+  if ( $self->get_status() == PLAYER_STATUS_PLAY ) {
+    $kernel->delay( 'check_finished', XINE_CHECK_INTERVAL_SECS );
+  }
+  else {
+    $kernel->post( 'Scheduler', 'finished' );
+  }
+}
+
+############################## Object Methods ################################
+
+##
+## spawn()
+##
+## Creates the appropriate Player session.
+##
+sub spawn {
+  my $self = shift;
+
+  POE::Session->create(
+		      object_states => [ $self => [ qw(_start _stop play check_finished) ] ]
+		     );
+}
+
+##
+## get_status()
+##
+## Returns one of:
+##   PLAYER_STATUS_PLAY if a movie is playing
+##   PLAYER_STATUS_STILL if a still image is on the screen
+##   PLAYER_STATUS_STOP if nothing is playing.
+##
+sub get_status {
+  my $self = shift;
+
+  return xine_simple_get_status();
+}
---
+++ PlaybackMachine/t/player.t
@@ -0,0 +1,68 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl 1.t'
+
+#########################
+
+use strict;
+
+use Test::More tests => 6;
+BEGIN { use_ok('Video::PlaybackMachine::Player') };
+
+
+## NOTE: Test movie from ftp.tek.com/tv/test/streams
+use constant TEST_MOVIE_1 => '/home/steven/dev/Video/PlaybackMachine/t/test_movies/time_015.m2v';
+
+#########################
+
+# Insert your test code below, the Test::More module is use()ed here so read
+# its man page ( perldoc Test::More ) for help writing this test script.
+
+use Video::PlaybackMachine::Player qw(PLAYER_STATUS_PLAY PLAYER_STATUS_STOP);
+use POE;
+use POE::Kernel;
+use POE::Session;
+
+##
+## Verifies that player is initially stopped, starts a
+## Xine movie, verifies the movie is running two seconds later.
+## Then it waits for the movie to quit. If the movie quits,
+## and make sure that it ran the right amount of time.
+##
+TESTS: {
+  my $player = Video::PlaybackMachine::Player->new();
+  $player->spawn();
+  -e TEST_MOVIE_1 or die "Couldn't find TEST_MOVIE_1\n";
+  POE::Session->create(
+		       inline_states => {
+					 _start => sub {
+					   my ($heap, $kernel) = @_[HEAP, KERNEL];
+					   $kernel->alias_set('Scheduler');
+					   $heap->{player} = Video::PlaybackMachine::Player->new();
+					   $heap->{player_session} = $heap->{player}->spawn();
+					   $kernel->delay('check_running', 2);
+					   $kernel->delay('terminate', 20);
+					   $heap->{time} = time();
+					   $kernel->post($heap->{player_session}, 'play', 0, TEST_MOVIE_1);
+					 },
+					 check_running => sub {
+					   my ($heap, $kernel) = @_[HEAP, KERNEL];
+					   ok( $heap->{player}->get_status() == PLAYER_STATUS_PLAY, "Player is playing");
+					 },
+					 finished => sub {
+					   my ($heap, $kernel) = @_[HEAP, KERNEL];
+					   $heap->{length} =  time() - $heap->{time};
+					   ok( $heap->{length} > 0, "Time ran forward. That's generally good.");
+					   ok( ( ($heap->{length} - 15) ** 2 ) < 5, "Ended more or less on time.");
+					   ok( $heap->{player}->get_status() == PLAYER_STATUS_STOP, "Player has stopped and has said so." );
+					 
+					 },
+					 terminate => sub {
+					   my ($heap, $kernel) = @_[HEAP, KERNEL];
+					   ok( exists $heap->{length} && defined $heap->{length}, "Player said that it finished.");
+					   exit(0);
+					 }
+					}
+		      );
+  POE::Kernel->run();
+}
+
