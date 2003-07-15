51508c17e17130fe6ddda4c2a4c1338aaa7d6394
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2003-07-15 17:36:14 +0000

    FixedTimeLayout is a TimeLayout that always returns a fixed time.

---
+++ PlaybackMachine/PlaybackMachine/TimeLayout/FixedTimeLayout.pm
@@ -0,0 +1,60 @@
+package Video::PlaybackMachine::TimeLayout::FixedTimeLayout;
+
+####
+#### Video::PlaybackMachine::TimeLayout::FixedTimeLayout
+####
+#### $Revision$
+####
+#### A TimeLayout that indicates that a certain FillProducer only
+#### wants to generate content for a fixed amount of time. 
+####
+
+use strict;
+use warnings;
+use Carp;
+
+############################# Class Constants #############################
+
+############################## Class Methods ##############################
+
+##
+## new()
+##
+## Arguments:
+##   TIME: int -- Amount of time for fixed layout to return
+##
+## Creates a new FixedTimeLayout that returns TIME.
+##
+sub new {
+  my $type = shift;
+  my ($time) = @_;
+
+  my $self = { time => $time };
+  bless $self, $type;
+}
+
+############################# Object Methods ##############################
+
+##
+## min_time()
+##
+## Returns the minimum amount of time the fill can take. In this case,
+## returns the fixed time.
+##
+sub min_time { 
+  return $_[0]->{time};
+}
+
+##
+## preferred_time()
+##
+## Arguments:
+##  TIME_LEFT
+##
+## Returns the fixed time.
+##
+sub preferred_time { 
+  return $_[0]->{time};
+}
+
+1;
