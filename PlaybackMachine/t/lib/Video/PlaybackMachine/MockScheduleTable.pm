3780efec6a77189a9be7a5910ac71dc83b9b6b1d
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2003-07-17 05:05:11 +0000

    Made modifiable.

--- PlaybackMachine/t/lib/Video/PlaybackMachine/MockScheduleTable.pm
+++ PlaybackMachine/t/lib/Video/PlaybackMachine/MockScheduleTable.pm
@@ -6,12 +6,13 @@ package Video::PlaybackMachine::MockScheduleTable;
 #### $Revision$
 ####
 #### Exports a single function letting us make a mock schedule table.
+#### (Also there's an object-oriented interface, plus an add() method
+#### you can use to add blank ScheduleEntries. The whole thing was 
+#### initially a Test::MockObject, thus the closure implementation.)
 ####
 
 use strict;
 use warnings;
-
-use Test::MockObject;
 
 use Video::PlaybackMachine::AVFile;
 use Video::PlaybackMachine::ScheduleEntry;
@@ -50,8 +51,53 @@ our @EXPORT_OK = qw(mock_schedule_table);
 sub mock_schedule_table {
   my ($now) = @_;
 
-  my $mock = Test::MockObject->new();
+  my $mst = Video::PlaybackMachine::MockScheduleTable->new($now);
+
+
+  # First one starts NOW and lasts 10 seconds till NOW+10
+  $mst->add(0,  0, 10);
+
+  # Then we fill for 10 seconds
+
+  # Second one starts NOW + 20 and lasts 7 seconds
+  $mst->add(20, 20,  4, 7);
+
+  # Third one wants to start at 25 seconds, but starts at 28, and lasts 9 seconds till NOW+37
+  $mst->add(25, 28,  9, 9);
+
+  # 3 seconds is beneath fill threshold
+
+  # Fourth one starts at 40 seconds and lasts till 20 seconds
+  $mst->add(40, 40,  5, 20);
+
+  # Fifth one wants to start at 46 seconds, but is skipped
+  $mst->add(46, -1, 15);
+
+  # Sixth one starts at 65 seconds
+  $mst->add(65, 65,  5);
+
+  return $mst;
+
+}
+
+##
+## new()
+##
+## Creates a new, empty MockScheduleTable.
+##
+sub new {
+  my $type = shift;
+  my ($now) = @_;
+
+  my @entries = ();
   my %entries_cache = ();
+
+  my $self = {};
+
+  $self->{'add_func'} = sub {
+    push @entries, [ @_ ];
+  };
+
 
   my $make_entry_func = sub {
     my ($num, $start_off,  $expected_off, $duration, $real_duration) = @_;
@@ -77,59 +123,49 @@ sub mock_schedule_table {
     return $entry;
   };
 
-  my @entries = (
-		 # First one starts NOW and lasts 10 seconds till NOW+10
-		 [0,  0, 10],
+  $self->{get_entries_after_func} =  sub {
+    my ($time) = @_;
 
-		 # Then we fill for 10 seconds
+    foreach my $idx (0 .. $#entries) {
+      my $sched_time = $entries[$idx][0] + $now;
+      if ( $sched_time > $time) {
+	return &$make_entry_func($idx + 1, @{ $entries[$idx] });
+      }
+    }
+    return;
+  };
 
-		 # Second one starts NOW + 20 and lasts 7 seconds
-		 [20, 20,  4, 7],
+  $self->{get_entry_during_func} = sub {
+    my ($time) = @_;
+    
+    foreach my $idx (0 .. $#entries) {
+      my $sched_start = $entries[$idx][0] + $now;
+      my $sched_end = $entries[$idx][2] + $sched_start;
+      if ( ( $sched_start <= $time ) && ( $sched_end > $time ) ) {
+	return &$make_entry_func($idx + 1, @{ $entries[$idx] });
+      }
+    }
+    
+    return;
+  };
 
-		 # Third one wants to start at 25 seconds, but starts at 28, and lasts 9 seconds till NOW+37
-		 [25, 28,  9, 9],
+  bless $self, $type;
 
-		 # 3 seconds is beneath fill threshold
+}
 
-		 # Fourth one starts at 40 seconds and lasts till 20 seconds
-		 [40, 40,  5, 20],
+sub get_entries_after {
+  my $self = shift;
+  return $self->{get_entries_after_func}(@_);
+}
 
-		 # Fifth one wants to start at 46 seconds, but is skipped
-		 [46, -1, 15],
+sub get_entry_during {
+  my $self = shift;
+  return $self->{get_entry_during_func}(@_);
+}
 
-		 # Sixth one starts at 65 seconds
-		 [65, 65,  5]
-		 );
-
-  $mock->mock('get_entries_after', sub {
-		my ($time) = $_[1];
-
-		foreach my $idx (0 .. $#entries) {
-		  my $sched_time = $entries[$idx][0] + $now;
-		  if ( $sched_time > $time) {
-		    return &$make_entry_func($idx + 1, @{ $entries[$idx] });
-		  }
-		}
-		return;
-	      }
-	     );
-
-  $mock->mock( 'get_entry_during', sub {
-		       my ($time) = $_[1];
-
-		       foreach my $idx (0 .. $#entries) {
-			 my $sched_start = $entries[$idx][0] + $now;
-			 my $sched_end = $entries[$idx][2] + $sched_start;
-			 if ( ( $sched_start <= $time ) && ( $sched_end > $time ) ) {
-			   return &$make_entry_func($idx + 1, @{ $entries[$idx] });
-			 }
-		       }
-
-		       return;
-		     });
-
-  return $mock;
-
+sub add {
+  my $self = shift;
+  $self->{add_func}(@_);
 }
 
 1;
