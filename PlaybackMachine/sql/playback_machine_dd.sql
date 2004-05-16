28480186499e6ed5a147c01531c2a633b3994c10
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2004-05-16 22:10:15 +0000

    Simplified.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- PlaybackMachine/sql/playback_machine_dd.sql
+++ PlaybackMachine/sql/playback_machine_dd.sql
@@ -2,18 +2,16 @@ CREATE TABLE content_types (
 	type		text primary key
 );
 
-CREATE SEQUENCE av_file_component_seq;
-
 CREATE TABLE av_file_component (
-	order	int NOT NULL,
-	file	text NOT NULL,
-	id	int primary key DEFAULT nextval('av_file_component_seq') 
+	order		int NOT NULL DEFAULT(0),
+	file		text NOT NULL,
+	title		text REFERENCES av_files,
+	duration	interval NOT NULL,
+	PRIMARY KEY (file, order)
 );
 
 CREATE TABLE av_files (
 	title		text primary key,
-	file		text not null,
-	duration	interval	
 );
 
 CREATE TABLE contents (
@@ -29,25 +27,14 @@ CREATE TABLE schedules (
 
 CREATE SEQUENCE schedule_id_seq;
 
-CREATE TABLE base_schedule (
-	id		int primary key DEFAULT nextval('schedule_id_seq'),
+CREATE TABLE content_schedule (
+	id			int primary key DEFAULT nextval('schedule_id_seq'),
+	title 		text not null references contents
 	schedule	text not null references schedules (name) ON DELETE CASCADE,
 	listed		boolean not null,
 	name		text not null,	
-	start_time	timestamp not null,
-	stop_time	timestamp not null CHECK (start_time < stop_time)
+	start_time	timestamp not null
 );
-
-CREATE TABLE content_schedule (
-	title 		text not null references contents
-) INHERITS (base_schedule);
-
-CREATE TABLE placeholder_schedule (
-) INHERITS (base_schedule);
-
-CREATE TABLE alarm_schedule (
-	message		text unique not null
-) INHERITS (base_schedule);
 
 CREATE TABLE fill_slide_categories (
 	category	text primary key
@@ -73,15 +60,11 @@ CREATE TABLE fill_slides (
 	frequency	interval not null
 ) INHERITS (fill_base);
 
-CREATE TABLE announcements (
-	announcement	text not null
-);
-
 CREATE FUNCTION check_overlap() RETURNS TRIGGER AS '
 BEGIN
-	IF EXISTS( SELECT id FROM base_schedule
+	IF EXISTS( SELECT id FROM content_schedule
 		WHERE schedule = NEW.schedule
-			AND ( 
+			AND (
 				-- Conflicts with something starting after
 				( NEW.stop_time >= start_time and NEW.stop_time <= stop_time ) or
 
@@ -104,13 +87,6 @@ END;
 CREATE TRIGGER content_check_overlap BEFORE INSERT OR UPDATE ON content_schedule
 	FOR EACH ROW EXECUTE PROCEDURE check_overlap();
 
-CREATE TRIGGER placeholder_check_overlap BEFORE INSERT OR UPDATE ON placeholder_schedule
-	FOR EACH ROW EXECUTE PROCEDURE check_overlap();
-
-CREATE TRIGGER alarm_check_overlap BEFORE INSERT OR UPDATE ON alarm_schedule
-	FOR EACH ROW EXECUTE PROCEDURE check_overlap();
-
-
 CREATE FUNCTION content_schedule_fix_name() RETURNS TRIGGER AS '
 BEGIN
 	IF NEW.name ISNULL THEN
@@ -123,14 +99,3 @@ END;
 CREATE TRIGGER content_schedule_fix_name BEFORE INSERT OR UPDATE ON content_schedule
 	FOR EACH ROW EXECUTE PROCEDURE content_schedule_fix_name();
 
-CREATE FUNCTION alarm_schedule_fix_name() RETURNS TRIGGER AS '
-BEGIN
-	IF NEW.name ISNULL THEN
-		NEW.name := NEW.message;
-	END IF;
-	RETURN NEW;
-END;
-' LANGUAGE 'plpgsql';
-
-CREATE TRIGGER alarm_schedule_fix_name BEFORE INSERT OR UPDATE ON alarm_schedule
-	FOR EACH ROW EXECUTE PROCEDURE alarm_schedule_fix_name();
