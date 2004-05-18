59bcfea587ebb9c95467bde85c8559d6b4577bae
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2004-05-18 15:48:16 +0000

    Further updates.

--- PlaybackMachine/PlaybackMachine/DatabaseWatcher.pm
+++ PlaybackMachine/PlaybackMachine/DatabaseWatcher.pm
@@ -12,6 +12,9 @@ use strict;
 use warnings;
 use diagnostics;
 
+
+use IO::Handle;
+
 use POE;
 use POE::Session;
 use POE::Kernel;
@@ -30,6 +33,55 @@ use POE::Kernel;
 sub new {
 	my $type = shift;
 	my %in = @_;
-	
-	
+	my $self = {
+		    dbh => $in{'dbh'},
+		    table_name => $in{'table_name'},
+		    session => $in{'session'},
+                    event => $in{'event'}
+		   };
 }
+
+###################### Session Methods ##########################
+
+sub _start {
+  my ($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
+
+  # Watch for incoming messages from the database
+  my $fd = $self->{'dbh'}->func('getfd');
+  my $fh = IO::Handle->new();
+  $fh->fdopen($fd)
+    or die "Couldn't open file descriptor '$fd': $!";
+  $kernel->select_read($fh, 'changed');
+  $heap->{'fh'} = $fh;
+}
+
+sub _stop {
+  my ($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
+  my $fh = $heap->{'fh'};
+  $kernel->select_read($fh);
+  delete $heap->{'fh'};
+  $fh->close();
+}
+
+sub changed {
+  my ($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
+
+  my $ret = $self->{'dbh'}->func('pg_notifies')
+    or return;
+  $ret->[0] eq $self->{'table_name'} or return;
+  
+}
+
+###################### Object Methods ###########################
+
+sub spawn {
+  my $self = shift;
+
+  POE::Session->create(
+		       object_states => [
+					 $self => [ qw(_start _stop changed) ]
+					]
+		      );
+}
+
+
