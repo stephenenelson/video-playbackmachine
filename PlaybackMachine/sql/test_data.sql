3b6140575923d3aafc71bb3e7e30fa7116eb5410
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2005-03-20 18:10:30 +0000

    Updated with latest test data.

--- PlaybackMachine/sql/test_data.sql
+++ PlaybackMachine/sql/test_data.sql
@@ -1,20 +1,103 @@
-DELETE FROM schedules WHERE name='Test Schedule';
-DELETE FROM av_files WHERE title='Test Pattern';
-DELETE FROM av_files WHERE title='False Alarms';
+--
+-- PostgreSQL database dump
+--
 
-INSERT INTO schedules (name) VALUES ('Test Schedule');
+SET client_encoding = 'SQL_ASCII';
+SET check_function_bodies = false;
 
-INSERT INTO av_files(title) VALUES('Test Pattern');
-INSERT INTO av_file_component (title, file, duration) VALUES('Test Pattern', '/home/steven/Video/PlaybackMachine/test_movies/time_015.avi', '00:00:15');
+SET SESSION AUTHORIZATION 'steven';
 
-INSERT INTO av_files(title) VALUES('False Alarms');
-INSERT INTO av_file_component (title, file, duration) VALUES('False Alarms', '/home/steven/Video/PlaybackMachine/test_movies/music_falsealarms2.avi', '00:06:22');
+SET search_path = public, pg_catalog;
+
+--
+-- Data for TOC entry 3 (OID 18785)
+-- Name: content_types; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY content_types ("type") FROM stdin;
+\.
 
 
-INSERT INTO content_schedule (id, schedule, listed, start_time, title)
-	VALUES (1, 'Test Schedule', TRUE,  'Feb 18, 2005 11:32:00 AM' , 'Test Pattern');
+--
+-- Data for TOC entry 4 (OID 18792)
+-- Name: av_files; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY av_files (title) FROM stdin;
+Son of Bambi
+SignOff
+Sign Off
+Qetg
+Time 015
+Nosferatu
+\.
 
 
-INSERT INTO content_schedule (id, schedule, listed, start_time, title)
-	VALUES (2, 'Test Schedule', TRUE,  'Feb 18, 2005 11:34:00 AM' , 'False Alarms');
+--
+-- Data for TOC entry 5 (OID 18799)
+-- Name: av_file_component; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY av_file_component (file, title, duration, sequence_no) FROM stdin;
+/home/steven/movies/fill_son_of_bambi.asf	Son of Bambi	00:01:50	0
+/usr/share/playback_machine/sign_off.avi	SignOff	00:00:20	0
+/home/steven/movies/sign_off.avi	Sign Off	00:00:20	0
+/home/steven/Video/PlaybackMachine/test_movies/short_QETG.mov	Qetg	00:04:45	0
+/home/steven/Video/PlaybackMachine/test_movies/time_015.avi	Time 015	00:00:15	0
+/home/steven/movies/movie_nosferatu.avi	Nosferatu	01:24:20	0
+\.
+
+
+--
+-- Data for TOC entry 6 (OID 18811)
+-- Name: fill_shorts; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY fill_shorts (title, group_name) FROM stdin;
+Son of Bambi	\N
+Time 015	\N
+\.
+
+
+--
+-- Data for TOC entry 7 (OID 18820)
+-- Name: contents; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY contents (title, "type", director, description) FROM stdin;
+SignOff	\N	\N	\N
+Sign Off	\N	\N	\N
+Qetg	\N	\N	\N
+Nosferatu	\N	\N	\N
+\.
+
+
+--
+-- Data for TOC entry 8 (OID 18831)
+-- Name: schedules; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY schedules (name) FROM stdin;
+Baycon 2005
+\.
+
+
+--
+-- Data for TOC entry 9 (OID 18840)
+-- Name: content_schedule; Type: TABLE DATA; Schema: public; Owner: steven
+--
+
+COPY content_schedule (id, title, schedule, listed, start_time) FROM stdin;
+11	Nosferatu	Baycon 2005	t	2005-02-22 18:31:00-08
+15	Qetg	Baycon 2005	t	2005-02-22 18:24:00-08
+\.
+
+
+--
+-- TOC entry 2 (OID 18838)
+-- Name: schedule_id_seq; Type: SEQUENCE SET; Schema: public; Owner: steven
+--
+
+SELECT pg_catalog.setval('schedule_id_seq', 15, true);
+
 
