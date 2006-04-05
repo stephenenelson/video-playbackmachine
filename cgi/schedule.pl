aa24c9188f80723919625c501fda559eecda0171
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2006-04-05 13:47:56 +0000

    Reformat as HTML

--- cgi/schedule.pl
+++ cgi/schedule.pl
@@ -8,6 +8,7 @@ use Date::Manip;
 use Video::PlaybackMachine::ScheduleTable::DB;
 
 use POSIX qw/ceil/;
+use CGI;
 
 ################################## Constants ##################################
 
@@ -31,24 +32,42 @@ MAIN: {
 
 	my $entry = shift @entries or exit;
 
+print <<EOF;
+
+<html>
+<head>
+<meta http-equiv="Content-Language" content="en" />
+<meta name="GENERATOR" content="PHPEclipse 1.0" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Schedule</title>
+</head>
+<body bgcolor="#FFFFFF" text="#000000" link="#FF9966" vlink="#FF9966" alink="#FFCC99">
+<h1>Schedule</h1>
+<table border="1">
+	<tr><th>Time</th>
+		<th>Friday</th>
+	</tr>
+EOF
+
 	DURING: while (1) {
-	  print starttime($curr_time), "\t";
+		print "	<tr>\n";
+	  	print "     <td>", starttime($curr_time), "</td>\n";
 	  if ( abs( $curr_time - $entry->get_start_time() ) < $Step_Interval ) {
-	    print $entry->getTitle(), "\t", ceil($entry->get_listing()->get_length() / $Step_Interval);
+	    print "     <td rowspan=\"", ceil($entry->get_listing()->get_length() / $Step_Interval), "\">", $entry->getTitle(), "</td>\n";
 	    $entry = shift @entries or last DURING;
 	  }
-	  print "\n";
+	  print "</tr>\n";
 	  $curr_time += $Step_Interval;
 	}
 
-	print "\n";
+	print "</tr>\n";
 
 	while ($curr_time < $end_time) {
-	    print starttime($curr_time), "\n";
+		print "	<tr>\n";
+	  	print "     <td>", starttime($curr_time), "</td>\n  </tr>";
 	    $curr_time += $Step_Interval;
 	}
 
-	print "\n";
 }
 
 sub starttime {
