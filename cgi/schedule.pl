02a41a1dc3cf61cd31e3426dddaa140d0f3cd7b4
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2006-04-05 06:05:37 +0000

    Working version, text-only

--- cgi/schedule.pl
+++ cgi/schedule.pl
@@ -3,32 +3,56 @@
 use strict;
 use warnings;
 
-use CGI;
 use Date::Manip;
 
 use Video::PlaybackMachine::ScheduleTable::DB;
 
+use POSIX qw/ceil/;
+
 ################################## Constants ##################################
+
+our $Step_Interval = 5 * 60;
 
 ################################# Main Program ################################
 
 MAIN: {
 
-	my $query = CGI->new();
+	
+	my $table = Video::PlaybackMachine::ScheduleTable::DB->new( schedule_name => "BayCon 2006" );
+	
+	my $start_time = UnixDate(ParseDate("20060526140000"), '%s')
+	  or die;
 
-	print $query->header();
-	
-	my $table = Video::PlaybackMachine::ScheduleTable::DB->new( schedule => "BayCon 2006" );
-	
-	my $start_time = 
-	
-	my @entries = $table->get_entries_after(0);
-	
-	foreach my $entry ( @entries ) {
-			print $entry->get_title();
+	my $end_time = UnixDate(ParseDate("200605292359"), '%s');
+
+	my @entries = $table->get_entries_after($start_time - 1, 100000);
+
+	my $curr_time = $start_time;
+
+	my $entry = shift @entries or exit;
+
+	DURING: while (1) {
+	  print starttime($curr_time), "\t";
+	  if ( abs( $curr_time - $entry->get_start_time() ) < $Step_Interval ) {
+	    print $entry->getTitle(), "\t", ceil($entry->get_listing()->get_length() / $Step_Interval);
+	    $entry = shift @entries or last DURING;
+	  }
+	  print "\n";
+	  $curr_time += $Step_Interval;
 	}
-	
-	
+
+	print "\n";
+
+	while ($curr_time < $end_time) {
+	    print starttime($curr_time), "\n";
+	    $curr_time += $Step_Interval;
+	}
+
+	print "\n";
 }
 
+sub starttime {
+  my ($time) = @_;
 
+  return UnixDate(ParseDateString('epoch ' . $time), '%a %T');
+}
