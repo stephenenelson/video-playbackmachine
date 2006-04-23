8a1952d80b3f2ff0f91d956751b2f92a64fa6505
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2006-04-23 23:11:53 +0000

    Added "schedule_xml.php"

--- php/install_php.sh
+++ php/install_php.sh
@@ -5,7 +5,7 @@ PHP_GROUP=apache
 CP_BIN=/bin/cp
 CHOWN_BIN=/bin/chown
 PHP_DEST=/var/www/html
-PHP_FILES="playback_add.php playback_delete.php playback_fills.php  playback_schedule.php playback_titles_add.php playback_titles.php playback_update_finish.php playback_update.php"
+PHP_FILES="schedule_xml.php playback_add.php playback_delete.php playback_fills.php  playback_schedule.php playback_titles_add.php playback_titles.php playback_update_finish.php playback_update.php"
 
 
 $CP_BIN --target-directory $PHP_DEST $PHP_FILES
