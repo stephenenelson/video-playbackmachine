2286346b060050933c0db7e2c08d955f4b054ab1
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2008-01-12 20:48:15 +0000

    No longer needed; binary install is in Makefile.PL

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-perl Makefile.PL
-make install
-cp bin/playback_machine.pl /usr/bin
-chmod a+x /usr/bin/playback_machine.pl
-cp bin/add_movie.pl /usr/bin
-chmod a+x /usr/bin/add_movie.pl
