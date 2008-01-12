0bcd94e8ae6260ce334b7cf27ca955b80728681f
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2008-01-12 21:11:20 +0000

    Took out build_rpm.sh

--- bin/playback_machine.pl
+++ bin/playback_machine.pl
@@ -8,96 +8,43 @@ use Getopt::Long;
 
 use Log::Log4perl;
 
-use FindBin '$Bin';
-use lib "$Bin/../lib";
-
 use Video::PlaybackMachine::Config;
-use Video::PlaybackMachine::ScheduleTable::DB;
-use Video::PlaybackMachine::DatabaseWatcher;
-use Video::PlaybackMachine::FillSegment;
-use Video::PlaybackMachine::Filler;
-use Video::PlaybackMachine::Scheduler;
-use Video::PlaybackMachine::FillProducer::SlideShow;
-use Video::PlaybackMachine::FillProducer::FillShort;
-use Video::PlaybackMachine::FillProducer::StillFrame;
-use Video::PlaybackMachine::FillProducer::UpNext;
-use Video::PlaybackMachine::FillProducer::NextSchedule;
+
+use Video::PlaybackMachine;
 
 our $config = Video::PlaybackMachine::Config->config();
 
-our $Skip_Tolerance = $config->skip_tolerance();
-
 MAIN: {
-	my ($date);
-	
+
+    if ( $config->daemonize() ) {
 	my $start_time = time();
 
 	while (1) {
-	
-		# Spawn off a child to do actual running
-		my $pid;
-		if ( my $pid = fork ) {
-			sleep 5;
-			wait;
-		}
-		else {
-			
-			open(STDERR, '>>' . $config->stderr_log())
-				or die "Couldn't open '" . $config->stderr_log() ."' for STDERR log: $!; stopped";
 
-			Log::Log4perl::init( \(
-				$config->log_config()
-                          )
-			);
+        # Spawn off a child to do actual running
+        my $pid;
+        if ( my $pid = fork ) {
+            sleep 5;
+            wait;
+        }
+        else {
 
+            open( STDERR, '>>' . $config->stderr_log() )
+              or die "Couldn't open '"
+              . $config->stderr_log()
+              . "' for STDERR log: $!; stopped";
 
-			my $schedule_name = $config->schedule();
+            Log::Log4perl::init( \( $config->log_config() ) );
 
-			my $table =
-			  Video::PlaybackMachine::ScheduleTable::DB->new(
-				schedule_name => $schedule_name, );
+            Video::PlaybackMachine->run($start_time);
+        }
 
-			my $offset = 0;
-			$date = $config->start();
+    }
 
-			if ( $config->offset() > 0 || defined($date) ) {
-				$offset = $config->offset() - ( time() - $start_time );				
-
-				if ( defined($date) ) {
-					if ( $date eq 'first' ) {
-						$offset += $table->get_offset_to_first() + 1;
-					}
-					else {
-						$offset += $table->get_offset($date);
-					}
-				}
-			
-			}
-			
-			my $watcher = Video::PlaybackMachine::DatabaseWatcher->new(
-				dbh     => $table->getDbh(),
-				table   => 'content_schedule',
-				session => 'Scheduler',
-				event   => 'update',
-			);
-			
-			my $watcher_session = $watcher->spawn();
-
-			my $scheduler = Video::PlaybackMachine::Scheduler->new(
-				skip_tolerance => $Skip_Tolerance,
-				schedule_table => $table,
-				filler         => $config->get_fill($table),
-				offset         => $offset,
-				watcher        => $watcher_session
-			);
-
-			$scheduler->spawn();
-
-			POE::Kernel->run();
-
-		}
-
-	}
+    }
+    else {
+	Video::PlaybackMachine->run();
+    }
 
 }
 
--- lib/Video/PlaybackMachine/FillProducer/FillShort.pm
+++ lib/Video/PlaybackMachine/FillProducer/FillShort.pm
@@ -5,12 +5,6 @@ package Video::PlaybackMachine::FillProducer::FillShort;
 ####
 #### $Revision$
 ####
-#### Plays a randomly-chosen short from the fill contents database
-#### that fits into the time available.
-#### 
-#### In a slightly scurvy trick, FillShort also defines its own time layout,
-#### which allows it to query the database whenever someone asks it.
-####
 
 # TODO: extract DB code from ScheduleTable::DB
 #
@@ -20,131 +14,157 @@ use strict;
 use warnings;
 use Carp;
 use POE;
+use POSIX 'INT_MAX';
 
-use base qw(Video::PlaybackMachine::FillProducer
-            Video::PlaybackMachine::TimeLayout
+use base qw(
+  Video::PlaybackMachine::FillProducer
+  Video::PlaybackMachine::TimeLayout
 );
+
 use Video::PlaybackMachine::TimeLayout::FixedTimeLayout;
 use Video::PlaybackMachine::AVFile;
 
-
 ########################### Class Constants #######################
-
 
 ############################ Class Methods ########################
 
-##
-## new()
-##
-## Arguments:
-##   TABLE -- ScheduleTable::DB
-##
-## Creates a new FillShort which uses the DBI connection given.
-##
 sub new {
-  my $type = shift;
-  my ($st) = @_;
-  my $self = {
-	      st => $st,
-	      seen => {}
-	     };
-  bless $self, $type;
+    my $type = shift;
+    my ($st) = @_;
+    my $self = {
+        st   => $st,
+        seen => {}
+    };
+    bless $self, $type;
 
 }
 
-##
-## get_time_layout()
-##
-## Returns:
-##   Video::PlaybackMachine::FillShort
-##
-## In a scurvy trick, we're our own time layout object. That way, the
-## time layout can query the database for minimum and preferred times.
-##
 sub get_time_layout {
-  return $_[0];
+    return $_[0];
 }
 
-##
-## start()
-##
-## Arguments:
-##  TIME: int -- time in seconds that we're to fill
-##
 sub start {
-  my $self = shift;
-  my ($time) = @_;
+    my $self = shift;
+    my ($time) = @_;
 
-  my @fills = $self->{'st'}->get_fills($time)
-    or return;
-  foreach my $avfile (@fills) {
-    my $file = $avfile->get_file();
-    $self->{seen}{$file}++ and next;
-    $poe_kernel->yield('short_ready', $file);
-    return 1;
-  }
-  $self->{'seen'} = {};
-  $self->start($time);
+    my @fills = $self->{'st'}->get_fills($time)
+      or return;
+    foreach my $avfile (@fills) {
+        my $file = $avfile->get_file();
+        $self->{seen}{$file}++ and next;
+        $poe_kernel->yield( 'short_ready', $file );
+        return 1;
+    }
+    $self->{'seen'} = {};
+    $self->start($time);
 }
 
-##
-## min_time()
-##
-## Returns the minimum amount of time this filler can run, i.e. the
-## shortest short. Caches its values to minimize database calls.
-##
 sub min_time {
-  my $self = shift;
+    my $self = shift;
 
-  unless (  defined $self->{'min_time'} ) {
+    unless ( defined $self->{'min_time'} ) {
 
-    defined($self->{'min_time'} = $self->{'st'}->get_min_fill())
-      or croak "No available shorts";
+        defined( $self->{'min_time'} = $self->{'st'}->get_min_fill() )
+          or return INT_MAX;
 
-  }
+    }
 
-  return $self->{'min_time'};
+    return $self->{'min_time'};
 }
 
-##
-## preferred_time()
-##
-## Returns the length of the longest fillmovie. (Sorry, it just got
-## too strange referring to the "longest short.") Possibly this
-## behavior is a bug in the architecture, but it's the only sane
-## deterministic value. Better behavior would be to return the length
-## of the movie which we will play next, but there's a random factor
-## there.
-##
 sub preferred_time {
-  my $self = shift;
+    my $self = shift;
 
-  unless ( defined $self->{'preferred_time'} ) {
-    defined($self->{'preferred_time'} = $self->{'st'}->get_max_fill())
-      or croak "No available shorts";
-  }
+    unless ( defined $self->{'preferred_time'} ) {
+        defined( $self->{'preferred_time'} = $self->{'st'}->get_max_fill() )
+          or return INT_MAX;
+    }
 
-  return $self->{'preferred_time'};
+    return $self->{'preferred_time'};
 }
 
-##
-## is_available()
-##
-## Returns true if there are shorts available to play.
-##
 sub is_available {
-  my $self = shift;
+    my $self = shift;
 
-  return defined($self->{'st'}->get_max_fill());
+    return defined( $self->{'st'}->get_max_fill() );
 }
 
-##
-## get_next()
-##
-## Returns the sequence number of the next short to play
-##
 sub get_next {
-  return $_[1] + 1;
-  
+    return $_[1] + 1;
+
 }
+
 1;
+
+__END__
+
+=head1 NAME
+
+Video::PlaybackMachine::FillProducer::FillShort -- Plays a randomly-chosen short from the fill contents database that fits into the time available.
+
+=head1 DESCRIPTION
+
+Plays a randomly-chosen short from the fill contents database that
+fits into the time available.
+
+In a slightly scurvy trick, FillShort also defines its own time
+layout, which allows it to query the database whenever someone asks
+it.
+
+=head1 METHODS
+
+=head2 Class Methods
+
+=head3 new()
+
+Arguments:
+
+   TABLE -- ScheduleTable::DB
+
+Creates a new FillShort which uses the DBI connection given.
+
+=head3 get_time_layout()
+
+Returns:
+
+   Video::PlaybackMachine::FillShort
+
+In a scurvy trick, we're our own time layout object. That way, the
+time layout can query the database for minimum and preferred times.
+
+
+=head3 start()
+
+Arguments:
+
+  TIME: int -- time in seconds that we're to fill
+
+=head3 min_time()
+
+Returns the minimum amount of time this filler can run, i.e. the
+shortest short. Caches its values to minimize database calls.
+
+If there is no filler at all, returns C<INT_MAX>.
+
+
+=head3 preferred_time()
+
+Returns the length of the longest fillmovie. (Sorry, it just got
+too strange referring to the "longest short.") Possibly this
+behavior is a bug in the architecture, but it's the only sane
+deterministic value. Better behavior would be to return the length
+of the movie which we will play next, but there's a random factor
+there.
+
+
+=head3 is_available()
+
+Returns true if there are shorts available to play.
+
+
+=head3 get_next()
+
+Returns the sequence number of the next short to play
+
+=cut
+
