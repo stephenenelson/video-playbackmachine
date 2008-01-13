94caf46190a0af52b8f82eec0668e55bb47759ab
steven <steven@e812f9b6-d40f-0410-a31f-b299d6ca8470>
2008-01-13 02:30:24 +0000

    Documented

--- lib/Video/PlaybackMachine/AVFile.pm
+++ lib/Video/PlaybackMachine/AVFile.pm
@@ -5,7 +5,6 @@ package Video::PlaybackMachine::AVFile;
 ####
 #### $Revision$
 ####
-#### An MPEG, PNG, OGG, or other file which can be played.
 ####
 
 use strict;
@@ -51,3 +50,46 @@ sub get_length { return $_[0]->{length}; }
 sub get_file { return $_[0]->{file}; }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Video::PlaybackMachine::AVFile - An MPEG, PNG, OGG, or other file which can be played
+
+=head1 SYNOPSIS
+
+  use Video::PlaybackMachine::AVFile;
+
+  my $avfile = Video::PlaybackMachine::AVFile->new($file, $duration);
+
+  my $filename = $avfile->get_file();
+  my $length = $avfile->get_length();
+
+=head1 DESCRIPTION
+
+Simple object representing a playable file.
+
+=head1 METHODS
+
+=head2 CLASS METHODS
+
+=head3 new()
+
+  new({file => $file, length => $length_secs });
+
+Constructor. The C<$file> parameter should be a filename. The
+C<$length> parameter should be the duration of the file in seconds.
+
+=head2 OBJECT METHODS
+
+=head3 get_length()
+
+Returns the duration of the AV file in seconds.
+
+=head3 get_file()
+
+Returns the filename.
+
+=cut
+
