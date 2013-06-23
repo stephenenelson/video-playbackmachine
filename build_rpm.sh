89dde1980a9dfca046dd8eb408498bcf4aac0d01
Stephen Nelson <stephenenelson@mac.com>
2013-06-23 15:50:40 -0700

    Move to dzil

--- build_rpm.sh
+++
@@ -1,1 +0,0 @@
-cpan2rpm --requires="postgresql-server, perl-DBD-Pg" --requires="perl-AppConfig, perl-Image-Imlib2, perl-Log-Log4perl, perl-POE, perl-Test-MockObject, perl-Time-Duration" --requires="perl-Video-Xine" --requires="perl-X11-FullScreen" --epilogue="install:mkdir -p %_sysconfdir/playback_machine && cp conf/playback_machine.conf conf/playback_log.conf $_sysconfdir/playback_machine/" .
