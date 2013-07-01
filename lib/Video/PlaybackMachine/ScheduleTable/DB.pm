f8a04357210cebb16d8eab779ab5fddee931974d
Stephen Nelson <stephenenelson@mac.com>
2013-06-30 17:00:27 -0700

    Perltidied ScheduleTable/DB.pm

--- lib/Video/PlaybackMachine/ScheduleTable/DB.pm
+++ lib/Video/PlaybackMachine/ScheduleTable/DB.pm
@@ -19,7 +19,6 @@ use Video::PlaybackMachine::ScheduleEntry;
 use Video::PlaybackMachine::Config;
 use Video::PlaybackMachine::DB;
 
-
 ############################## Class Methods #######################################
 
 ##
@@ -27,15 +26,14 @@ use Video::PlaybackMachine::DB;
 ##
 ## Creates a new ScheduleTable::DB object.
 ##
-sub new
-{
-  my $type = shift;
-  my $self = {
-	      schedule_name => Video::PlaybackMachine::Config->config->schedule(),
-	      dbh           => undef,
-	      logger        => Log::Log4perl->get_logger('Video.PlaybackMachine.DB'),
-	     };
-  bless $self, $type;
+sub new {
+    my $type = shift;
+    my $self = {
+        schedule_name => Video::PlaybackMachine::Config->config->schedule(),
+        dbh           => undef,
+        logger        => Log::Log4perl->get_logger('Video.PlaybackMachine.DB'),
+    };
+    bless $self, $type;
 }
 
 ############################## Object Methods ######################################
@@ -51,28 +49,28 @@ sub getDbh { return Video::PlaybackMachine::DB->db(); }
 ##
 ## Returns all entries which start or end between BEGIN_TIME and END_TIME.
 ##
-sub get_entries_between
-{
-	my $self = shift;
-	my ($begin_time, $end_time) = @_;
-	
-	my $schema = Video::PlaybackMachine::DB->schema();
-	
-	my $entries_rs = $schema->resultset('ScheduleEntry')->search(
-		{
-			[ { 'start_time' => { '>', $begin_time } },
-			  { 'stop_time' => { '>', $begin_time } },
-			],
-			'start_time' => {'<', $end_time },
-			'schedule' => $self->{'schedule_name'}
-		},
-		{
-			'order_by' => 'start_time'
-		}
-	);
-	
-	return $entries_rs->all();
-	
+sub get_entries_between {
+    my $self = shift;
+    my ( $begin_time, $end_time ) = @_;
+
+    my $schema = Video::PlaybackMachine::DB->schema();
+
+    my $entries_rs = $schema->resultset('ScheduleEntry')->search(
+        {
+            [
+                { 'start_time' => { '>', $begin_time } },
+                { 'stop_time'  => { '>', $begin_time } },
+            ],
+            'start_time' => { '<', $end_time },
+            'schedule'   => $self->{'schedule_name'}
+        },
+        {
+            'order_by' => 'start_time'
+        }
+    );
+
+    return $entries_rs->all();
+
 }
 
 ##
@@ -85,45 +83,42 @@ sub get_entries_between
 ## Returns all entries which start after a given time. In scalar context,
 ## returns the first entry after the given time. Returns undef if nothing left.
 ##
-sub get_entries_after
-{
-  my $self = shift;
-  my ($time, $num_entries) = @_;
+sub get_entries_after {
+    my $self = shift;
+    my ( $time, $num_entries ) = @_;
 
-  defined $num_entries
-    or $num_entries = 1;
+    defined $num_entries
+      or $num_entries = 1;
 
-  # Get next content_schedule entry
-  
-  my $sth = $self->getDbh()->prepare(
-q/
+    # Get next content_schedule entry
+
+    my $sth = $self->getDbh()->prepare(
+        q/
   	SELECT title, start_time
 	FROM schedule_times_raw
 	WHERE start_time > ? AND schedule = ?
         ORDER BY start_time
 	LIMIT ?
  /
-	  );
-  $sth->execute(
-		$time,
-		$self->{'schedule_name'},
-		$num_entries
-	       )
-    or $self->{'logger'}->logdie($DBI::errstr);
-  
-  my @entries = ();
+    );
+    $sth->execute( $time, $self->{'schedule_name'}, $num_entries )
+      or $self->{'logger'}->logdie($DBI::errstr);
 
-  while (my ( $title, $start_time, $description ) = $sth->fetchrow_array()) {
-    push(@entries, $self->_entry_for( $title, $start_time, $description ));
-  }
+    my @entries = ();
 
-  if (wantarray) {
-    return @entries;
-  }
-  else {
-    return $entries[0] if $num_entries == 1;
-    return \@entries;
-  }
+    while ( my ( $title, $start_time, $description ) = $sth->fetchrow_array() )
+    {
+        push( @entries,
+            $self->_entry_for( $title, $start_time, $description ) );
+    }
+
+    if (wantarray) {
+        return @entries;
+    }
+    else {
+        return $entries[0] if $num_entries == 1;
+        return \@entries;
+    }
 }
 
 ##
@@ -137,8 +132,8 @@ q/
 ##
 # TODO A bit goofy to fetch duration, then do nothing with it?
 sub get_fills {
-  my $self = shift;
-  my ($time) = @_;
+    my $self = shift;
+    my ($time) = @_;
 
     my $sth = $self->getDbh()->prepare(<<EOF);
        SELECT title, duration
@@ -146,54 +141,54 @@ sub get_fills {
        WHERE duration < ?
        ORDER BY random()
 EOF
-  $sth->execute("$time seconds")
-    	  or die $DBI::errstr;
+    $sth->execute("$time seconds")
+      or die $DBI::errstr;
 
-  my @avfiles = ();
-  my ($title, $duration);
-  while ( ($title, $duration) = $sth->fetchrow_array() ) {
-    push(@avfiles, $self->_avfiles_for($title));
-  }
-  return @avfiles;
+    my @avfiles = ();
+    my ( $title, $duration );
+    while ( ( $title, $duration ) = $sth->fetchrow_array() ) {
+        push( @avfiles, $self->_avfiles_for($title) );
+    }
+    return @avfiles;
 }
 
 ##
 ## Returns the length of the shortest fillmovie.
 ##
 sub get_min_fill {
-  my $self = shift;
+    my $self = shift;
 
-  my $sth = $self->getDbh()->prepare(<<EOF);
+    my $sth = $self->getDbh()->prepare(<<EOF);
   SELECT min(date_part('epoch', avfile_duration(title)))
       FROM fill_shorts
 EOF
-      $sth->execute()
-    	  or die $DBI::errstr;
+    $sth->execute()
+      or die $DBI::errstr;
 
-  my ($min_time) = $sth->fetchrow_array()
+    my ($min_time) = $sth->fetchrow_array()
       or return;
 
-  return $min_time;
+    return $min_time;
 }
 
 ##
 ## Returns the length of the longest fillmovie.
 ##
 sub get_max_fill {
-  my $self = shift;
+    my $self = shift;
 
-  my $sth = $self->getDbh()->prepare(<<EOF);
+    my $sth = $self->getDbh()->prepare(<<EOF);
   SELECT max(date_part('epoch', avfile_duration(title)))
      FROM fill_shorts
 EOF
 
-  $sth->execute()
-    or die $DBI::errstr;
+    $sth->execute()
+      or die $DBI::errstr;
 
-  my ($max_time) = $sth->fetchrow_array()
-    or return;
+    my ($max_time) = $sth->fetchrow_array()
+      or return;
 
-  return $max_time;
+    return $max_time;
 
 }
 
@@ -202,14 +197,14 @@ EOF
 ## timestamp format.
 ##
 sub db_format_time {
-  my $self = shift;
-  my ($time) = @_;
+    my $self = shift;
+    my ($time) = @_;
 
-  # Shim for time zone issue
-  my $corr_time = $time;
+    # Shim for time zone issue
+    my $corr_time = $time;
 
-  my $pd_time = ParseDateString("epoch $corr_time");
-  return UnixDate($pd_time, '%m-%d-%Y %T');
+    my $pd_time = ParseDateString("epoch $corr_time");
+    return UnixDate( $pd_time, '%m-%d-%Y %T' );
 
 }
 
@@ -217,16 +212,16 @@ sub db_format_time {
 ## Returns the database's idea of the current schedule time.
 ##
 sub db_schedule_time {
-  my $self = shift;
-  my ($offset) = @_;
+    my $self = shift;
+    my ($offset) = @_;
 
-  defined $offset or $offset = 0;
+    defined $offset or $offset = 0;
 
-  my $sth = $self->getDbh()->prepare('select now() - interval ?');
-  $sth->execute("$offset secs") or return;
-  my ($formatted_sched_time) = $sth->fetchrow_array()
-    or return;
-  return $formatted_sched_time;
+    my $sth = $self->getDbh()->prepare('select now() - interval ?');
+    $sth->execute("$offset secs") or return;
+    my ($formatted_sched_time) = $sth->fetchrow_array()
+      or return;
+    return $formatted_sched_time;
 
 }
 
@@ -235,72 +230,78 @@ sub db_schedule_time {
 ## and TIME. Returns undef if the time wasn't readable.
 ##
 sub get_offset {
-  my $self = shift;
-  my ($date) = @_;
+    my $self = shift;
+    my ($date) = @_;
 
-  my $sth = $self->getDbh()->prepare("select EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - TIMESTAMPTZ '$date')");
-  $sth->execute()
-    or return;
-  my ($offset) = $sth->fetchrow_array()
-    or return;
-  return $offset;
+    my $sth =
+      $self->getDbh()
+      ->prepare(
+        "select EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - TIMESTAMPTZ '$date')");
+    $sth->execute()
+      or return;
+    my ($offset) = $sth->fetchrow_array()
+      or return;
+    return $offset;
 }
 
 ##
 ## Returns the offset to the first schedule entry.
 ##
 sub get_offset_to_first {
-  my $self = shift;
+    my $self = shift;
 
-  my $sth = $self->getDbh()->prepare('SELECT EXTRACT( EPOCH from CURRENT_TIMESTAMP - start_time ) FROM schedule_times ORDER BY start_time LIMIT 1');
-  $sth->execute()
-    or return;
-  my ($offset) = $sth->fetchrow_array()
-    or return;
-  return $offset;
+    my $sth =
+      $self->getDbh()
+      ->prepare(
+'SELECT EXTRACT( EPOCH from CURRENT_TIMESTAMP - start_time ) FROM schedule_times ORDER BY start_time LIMIT 1'
+      );
+    $sth->execute()
+      or return;
+    my ($offset) = $sth->fetchrow_array()
+      or return;
+    return $offset;
 }
 
 ##
 ## Returns a list of av files for a given title.
 ##
 sub _avfiles_for {
-  my $self = shift;
-  my ($title) = @_;
-  
-  # Get file entries for next content_schedule entry
-  my $sth = $self->getDbh()->prepare(<<EOF);
+    my $self = shift;
+    my ($title) = @_;
+
+    # Get file entries for next content_schedule entry
+    my $sth = $self->getDbh()->prepare(<<EOF);
 SELECT
     file,
     date_part('epoch', duration) 
  FROM av_file_component
  WHERE title = ? ORDER BY sequence_no
 EOF
-  $sth->execute($title);
-  my @av_files = ();
-  while ( my ( $file, $duration ) = $sth->fetchrow_array() ) {
-      push(
-	   @av_files,
-	   Video::PlaybackMachine::AVFile->new( $file, $duration ) 
-	  );
+    $sth->execute($title);
+    my @av_files = ();
+    while ( my ( $file, $duration ) = $sth->fetchrow_array() ) {
+        push( @av_files,
+            Video::PlaybackMachine::AVFile->new( $file, $duration ) );
     }
 
-  return @av_files;
+    return @av_files;
 }
 
 sub _entry_for {
-  my $self = shift;
-  my ( $title, $start_time, $description ) = @_;
-  
-  my @av_files = $self->_avfiles_for($title);
-  
-  # Create the schedule entry
-  my $movie = Video::PlaybackMachine::Movie->new(
-						 title       => $title,
-						 description => $description,
-						 av_files    => \@av_files
-						);
+    my $self = shift;
+    my ( $title, $start_time, $description ) = @_;
 
-  return Video::PlaybackMachine::ScheduleEntry->new( int($start_time), $movie );
+    my @av_files = $self->_avfiles_for($title);
+
+    # Create the schedule entry
+    my $movie = Video::PlaybackMachine::Movie->new(
+        title       => $title,
+        description => $description,
+        av_files    => \@av_files
+    );
+
+    return Video::PlaybackMachine::ScheduleEntry->new( int($start_time),
+        $movie );
 }
 
 ##
@@ -313,38 +314,37 @@ sub _entry_for {
 ## Returns an empty list / undef if there is no scheduled program taking place
 ## at the given time.
 ##
-sub get_entry_during
-{
-	my $self = shift;
-	my ($time) = @_;
+sub get_entry_during {
+    my $self = shift;
+    my ($time) = @_;
 
-	  # Get next content_schedule entry
-	  my $sth = $self->getDbh()->prepare(
-		qq{
+    # Get next content_schedule entry
+    my $sth = $self->getDbh()->prepare(
+        qq{
   	SELECT title, start_time, description
 	FROM schedule_times_raw
 	WHERE ? BETWEEN start_time AND stop_time
 	AND schedule = ?
 	LIMIT 1
   }
-	  );
-	$sth->execute($time, $self->{'schedule_name'});
+    );
+    $sth->execute( $time, $self->{'schedule_name'} );
 
-	my ( $title, $start_time, $description ) = $sth->fetchrow_array()
-	  or return;
-	  
-	 return $self->_entry_for($title, $start_time, $description); 
+    my ( $title, $start_time, $description ) = $sth->fetchrow_array()
+      or return;
+
+    return $self->_entry_for( $title, $start_time, $description );
 
 }
 
 sub get_schedule_name {
-  return $_[0]->{'schedule_name'};
+    return $_[0]->{'schedule_name'};
 }
 
 sub finished {
-  my $self = shift;
-  $self->{'logger'}->debug('Disconnecting from database');
-  $self->getDbh()->disconnect();
+    my $self = shift;
+    $self->{'logger'}->debug('Disconnecting from database');
+    $self->getDbh()->disconnect();
 }
 
 1;
