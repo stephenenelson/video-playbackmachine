c529ed33e5c8684daba933fe1a8a15b94f644706
Stephen Nelson <stephenenelson@mac.com>
2013-07-20 09:41:48 -0700

    Dump the SQL for the DBIx::Class schema

---
+++ bin/create_schema.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Video::PlaybackMachine::DB;
+
+my $schema = Video::PlaybackMachine::DB->schema();
+
+$schema->create_ddl_dir(['SQLite'], '0.01', './', undef, { 'add_drop_table' => 0 });
